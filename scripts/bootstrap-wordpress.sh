1caaf37b356b0402e20aac6915bc2064a19578d3
Patrik Majer <patrik.majer.pisek@gmail.com>
2019-11-16 14:52:33 +0100

    vagrant: fix wp bootstrap

--- scripts/bootstrap-wordpress.sh
+++ scripts/bootstrap-wordpress.sh
@@ -4,9 +4,9 @@ STACKNAME="wordpressstack"
 
 echo "Bootstrap wordpress..."
 
-sleep 10
+sleep 30
 
-docker run -i --rm \
+docker run --rm \
     --volumes-from wordpress \
     --network ${STACKNAME}_app-network \
     wordpress:cli core install --path=/var/www/html --url=127.0.0.1:8080 --title=test --admin_user=test --admin_password=test --admin_email=test@example.com
